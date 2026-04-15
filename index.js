a4b6a303ceb8bbb36e4e66f91ffe40aba7f8242c
Ciro Florentin <cfloren01@gmail.com>
2026-04-15 20:37:33 -0300

    Update PORT configuration in index.js to default to 3000 if not set in environment variables

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const productos = require('./data/productos.json');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT;
-
-console.log(PORT);
+const PORT = process.env.PORT || 3000;
 
 //PRODUCTOS
 app.get('/productos', (req, res) => {
