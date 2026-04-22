c5cb226dc848fa7e77595207dbf3713fc01eb37a
CiroFlorentin <cfloren01@gmail.com>
2026-04-22 20:42:05 -0300

    clase 5

--- index.js
+++ index.js
@@ -1,72 +1,150 @@
+// const express = require('express');
+// const productos = require('./data/productos.json');
+// require('dotenv').config();
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// //PRODUCTOS
+// app.get('/productos', (req, res) => {
+//   res.status(200).json({
+//     message: 'Productos',
+//     status: 'success',
+//     data: productos,
+//   });
+// });
+// app.get('/productos/:idProducto', (req, res) => {
+//   const { idProducto } = req.params;
+//   if (isNaN(idProducto)) {
+//     res.status(400).json({
+//       message: 'ID no válido',
+//       status: 'error',
+//       data: null,
+//     });
+//   } else {
+//     const producto = productos.find((p) => p.id === Number(idProducto));
+//     if (!producto) {
+//       res.status(404).json({
+//         message: 'Producto no encontrado',
+//         status: 'error',
+//         data: null,
+//       });
+//     } else {
+//       res.status(200).json({
+//         message: 'Producto encontrado',
+//         status: 'success',
+//         data: producto,
+//       });
+//     }
+//   }
+// });
+
+// //CATEGORIAS
+// app.get('/categorias', (req, res) => {
+//   const categorias = productos.flatMap((p) => p.categorias);
+//   res.status(200).json({
+//     message: 'Categorias',
+//     status: 'success',
+//     data: [...new Set(categorias)],
+//   });
+// });
+
+// app.get('/categorias/find', (req, res) => {
+//   const { categoria } = req.query;
+//   const cat = productos.filter((p) => p.categorias.includes(categoria));
+
+//   if (cat.length === 0) {
+//     res.status(404).json({
+//       message: 'Categoria no encontrada',
+//       status: 'error',
+//       data: null,
+//     });
+//   } else {
+//     res.status(200).json({
+//       message: 'Productos por categoria',
+//       status: 'success',
+//       data: cat,
+//     });
+//   }
+// });
+
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(`Error al iniciar el servidor: ${err}`);
+//     process.exit(1);
+//   }
+//   console.log(`La aplicación está corriendo en el puerto ${PORT}`);
+// });
+
+// Clase Pre-Parcial----------------------------------------------
+// MÉTODO (OK) | (ERROR)
+// GET (200) | (404)
+// POST (201) | (400)
+// DELETE (200) | (404)
+// PUT (200) | (400) (404)
+
+// Https ->(protocolo)\\localhost:puerto
+// Puertos por default 80
+// Https : 443
+
+// INSTALAR EXPRESS
+//Creación de un servidor
+
+const materias = require('./data/materia.json');
 const express = require('express');
-const productos = require('./data/productos.json');
-require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
+app.use(express.json()); // Define el formato json para recibir información en un POST
+// ENDPOINTS -----------------------------------
+// app.verbo(ruta,función)
 
-//PRODUCTOS
-app.get('/productos', (req, res) => {
-  res.status(200).json({
-    message: 'Productos',
-    status: 'success',
-    data: productos,
-  });
-});
-app.get('/productos/:idProducto', (req, res) => {
-  const { idProducto } = req.params;
-  if (isNaN(idProducto)) {
+app.post('/materias', (req, res) => {
+  const { cod, nombre } = req.body;
+  if (!cod || !nombre) {
     res.status(400).json({
-      message: 'ID no válido',
+      message: 'Faltan datos',
       status: 'error',
       data: null,
     });
   } else {
-    const producto = productos.find((p) => p.id === Number(idProducto));
-    if (!producto) {
-      res.status(404).json({
-        message: 'Producto no encontrado',
-        status: 'error',
-        data: null,
-      });
-    } else {
-      res.status(200).json({
-        message: 'Producto encontrado',
-        status: 'success',
-        data: producto,
-      });
-    }
+    materias.push({
+      id: Math.floor(Math.random() * (300 - 1 + 1)) + 1,
+      cod,
+      nombre,
+    });
+    res.status(201).json({
+      message: 'Materia agregada',
+      status: 'success',
+      data: { cod, nombre },
+    });
   }
 });
-
-//CATEGORIAS
-app.get('/categorias', (req, res) => {
-  const categorias = productos.flatMap((p) => p.categorias);
+app.get('/materias/:id', (req, res) => {
+  const { id } = req.params;
+  const materia = materias.filter((e) => e.id.includes(id));
+  if (!materia) {
+    res.status(404).json({
+      message: 'Materia no encontrada',
+      status: 'error',
+      data: null,
+    });
+    return;
+  }
   res.status(200).json({
-    message: 'Categorias',
+    message: 'Materia encontrada',
     status: 'success',
-    data: [...new Set(categorias)],
+    data: materia,
   });
 });
 
-app.get('/categorias/find', (req, res) => {
-  const { categoria } = req.query;
-  const cat = productos.filter((p) => p.categorias.includes(categoria));
-
-  if (cat.length === 0) {
-    res.status(404).json({
-      message: 'Categoria no encontrada',
-      status: 'error',
-      data: null,
-    });
-  } else {
-    res.status(200).json({
-      message: 'Productos por categoria',
-      status: 'success',
-      data: cat,
-    });
-  }
+app.get('/materias', (req, res) => {
+  res.status(200).json({
+    message: 'Materias',
+    status: 'success',
+    data: materias,
+  });
 });
 
+// FIN ENDPOINTS -----------------------------------
 app.listen(PORT, (err) => {
   if (err) {
     console.log(`Error al iniciar el servidor: ${err}`);
